b61a35b10c15184214ce1415cf8700446b3ca8d2
beegeiger <beegeiger@gmail.com>
2019-10-03 19:21:07 -0700

    Still Working on Oauth

--- besafe.py
+++ besafe.py
@@ -79,19 +79,24 @@ def go_home():
 @app.route('/callback')
 def callback_handling():
     # Handles response from token endpoint
+    print('callback called')
     auth0.authorize_access_token()
+    print('callback called a')
     resp = auth0.get('userinfo')
+    print('callback called b')
     userinfo = resp.json()
-
+    print('callback called2')
     # Store the user information in flask session.
     session['jwt_payload'] = userinfo
+    print('callback called3')
     session['profile'] = {
         'user_id': userinfo['sub'],
         'name': userinfo['name'],
         'picture': userinfo['picture'],
         'email': userinfo['email']
     }
-    return redirect('/')
+    print('got through call back')
+    return redirect('/dashboard')
 
 @app.route('/dashboard')
 @requires_auth
@@ -100,111 +105,19 @@ def dashboard():
                            userinfo=session['profile'],
                            userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
 
-@app.route("/register", methods=["GET"])
-def register_form():
-    """Goes to registration Form. (Tested)"""
-
-    """Creating empty strings to send through jinja so that if someone is redirected
-     from /register(POST), their data will still be in the registration form"""
-    email_input = ""
-    username = ""
-    fname = ""
-    lname = ""
-    about_me = ""
-
-    #Renders registration page with empty form variables
-    return render_template("register.html", email=email_input, username=username,
-                           fname=fname, lname=lname, about_me=about_me)
-
-
-@app.route("/register", methods=["POST"])
-def register_process():
-    """Registration Form. (Tested)"""
-
-    """Creating empty strings in case there aren't already
-                data being passed from the registration redirect"""
-    fname = ""
-    lname = ""
-
-    #Sets variables equal to the form values
-    email_input = request.form['email_input']
-    email2 = request.form['email_input2']
-    phone = request.form['phone']
-
-    pw_input = request.form['password']
-    password2 = request.form['password2']
-
-    username = request.form['username']
-    tagline = request.form['tagline']
-    location = request.form['location']
-    p_word = bytes(pw_input, 'utf-8')
-    hashed_word = bcrypt.hashpw(p_word, bcrypt.gensalt()).decode('utf-8')
-
-    #These two categories need to be worked out better
-    # user_type = request.form['user_type']
-    # second_type = request.form['2nd']
-    
-    timezone = request.form['timezone']
-
-
-    if pw_input != password2:
-        flash("Your passwords don't match!")
-        return render_template("register.html", email=email_input, username=username, fname=fname,
-                               lname=lname, about_me=about_me)
-
-    """Checks to make sure values exist in the optional fields
-                before setting the variables equal to the form values"""
-    if len(request.form['fname']) >= 1:
-        fname = request.form['fname']
-    if len(request.form['lname']) >= 1:
-        lname = request.form['lname']
-
-
-    #Checking that the e-mail address field at least includes a "." and a "@"
-    if "." not in email_input or "@" not in email_input:
-        flash(email_input + " is not a valid e-mail address!")
-        return render_template("register.html", email=email_input, username=username,
-                               fname=fname, lname=lname, about_me=about_me)
-
-    #Checking that the e-mail address hasn't already been registered
-    elif User.query.filter_by(email=email_input).all() != []:
-        flash(email_input + """This e-mail has already been registered! Either sign in with it,
-                use a different e-mail address, or reset your password if you forgot it.""")
-        return render_template("register.html", email=email_input, username=username, fname=fname,
-                               lname=lname, about_me=about_me)
-
-    #Checking that the username is available
-    elif User.query.filter_by(username=username).all() != []:
-        flash(email_input + "This username is already in use! Please try another one!")
-        return render_template("register.html", email=email_input, username=username, fname=fname,
-                               lname=lname, about_me=about_me)
-
-    #Checking that the password length is at least 6
-    elif len(pw_input) < 6:
-
-        flash("Your password must be at least 5 characters long! Try again.")
-        return render_template("register.html", email=email_input, username=username, fname=fname,
-                               lname=lname, about_me=about_me)
-
-    #Otherwise, the new user's information is added to the database
-    else:        
-        new_user = User(email=email_input, password=hashed_word, username=username, fname=fname,
-                        lname=lname, description=about_me, tagline=tagline, location=location,
-                        email2=email2, phone=phone, timezone=timezone)
-        db.session.add(new_user)
-        db.session.commit()
-    return redirect('/login')
 
 @app.route("/login", methods=["GET"])
 def log_in():
     """Render's the log-in page if user not in session,
      otherwise redirects to the homepage (Tested)"""
-
+    print('login visited')
     # if 'current_user' in list(session.keys()):
     #     return redirect("/")
     # else:
     # return render_template("login.html")
-    return auth0.authorize_redirect(redirect_uri='https://d5d2bbc1.ngrok.io/callback', audience='https://dev-54k5g1jc.auth0.com/userinfo')
+    uri = 'http://localhost:5000/callback'
+    print(type(uri))
+    return auth0.authorize_redirect(redirect_uri=uri, audience='https://dev-54k5g1jc.auth0.com/userinfo')
 
 
 @app.route("/login", methods=["POST"])
