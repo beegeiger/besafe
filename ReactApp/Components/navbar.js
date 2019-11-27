78417c57a2b67f008a841738bc8a86511ec31b35
beegeiger <beegeiger@gmail.com>
2019-11-26 16:32:05 -0800

    Added Menu Buttons

--- ReactApp/Components/navbar.js
+++ ReactApp/Components/navbar.js
@@ -11,35 +11,17 @@ class NavBar extends React.Component {
     render() {
         return (
             <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-            <Menu.Item key="mail">
-              <Icon type="mail" />
-              Navigation One
+            <Menu.Item key="home">
+              BeSafe Home
             </Menu.Item>
-            <Menu.Item key="app" disabled>
-              <Icon type="appstore" />
-              Navigation Two
+            <Menu.Item key="contacts">
+              Contacts
             </Menu.Item>
-            <SubMenu
-              title={
-                <span className="submenu-title-wrapper">
-                  <Icon type="setting" />
-                  Navigation Three - Submenu
-                </span>
-              }
-            >
-              <Menu.ItemGroup title="Item 1">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
-              </Menu.ItemGroup>
-              <Menu.ItemGroup title="Item 2">
-                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                <Menu.Item key="setting:4">Option 4</Menu.Item>
-              </Menu.ItemGroup>
-            </SubMenu>
-            <Menu.Item key="alipay">
-              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                Navigation Four - Link
-              </a>
+            <Menu.Item key="profile">
+              User Profile
+            </Menu.Item>
+            <Menu.Item key="login">
+              Login
             </Menu.Item>
           </Menu>
         )
