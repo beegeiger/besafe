98ea36135ebd04e02e005135db51c88bf8707959
beegeiger <beegeiger@gmail.com>
2019-12-03 16:05:25 -0800

    Working on Displaying All Contacts

--- ReactApp/Components/contactform.js
+++ ReactApp/Components/contactform.js
@@ -24,7 +24,7 @@ export class ContactForm extends React.Component{
     handleSubmit(e) {
         e.preventDefault();
         var formData = new FormData(form)
-        let uri = "prefix" + "/contacts"
+        let uri = prefix + "/contacts"
         fetch(url, {
             method: 'post',
             body: formData,
@@ -35,41 +35,6 @@ export class ContactForm extends React.Component{
         const  autoCompleteResult  = this.state;
         return (
             <div>
-                {/* <Form>
-                    <Form.Item label="Contact Name">
-                        {getFieldDecorator('name', {
-                            rules: [
-                            {
-                                type: 'string',
-                                message: 'You Must add a Contact Name!',
-                            },
-                            {
-                                required: true,
-                                message: 'You Must add a Contact Name!',
-                            },
-                            ],
-                        })(<Input />)}
-                    </Form.Item>
-                    <Form.Item label="E-mail">
-                        {getFieldDecorator('email', {
-                            rules: [
-                            {
-                                type: 'email',
-                                message: 'The input is not valid E-mail!',
-                            },
-                            {
-                                required: false,
-                                message: 'Please input contact E-mail!',
-                            },
-                            ],
-                        })(<Input />)}
-                    </Form.Item>
-                    <Form.Item label="Phone Number">
-                        {getFieldDecorator('phone', {
-                            rules: [{ required: false, message: 'Please input your contact number!' }],
-                        })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
-                    </Form.Item>
-                </Form> */}
 
             <form>
                 <b>Contact Name:</b> <input type='textbox' name="name"></input><br />
--- ReactApp/Components/contacts.js
+++ ReactApp/Components/contacts.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Contact } from './contact';
+import { prefix } from "../urlprefix"
 
 export class Contacts extends React.Component{
+    componentWillMount() {
+        let uri = prefix + "/contacts"
+        fetch(url, {
+            method: 'post',
+            body: formData,
+        });
+      }
     render() {
         const contacts = {% contacts %}
         return(      
