4d378650883e9381c8502c80b5832709cd5e8f3a
beegeiger <beegeiger@gmail.com>
2021-03-10 16:03:28 -0800

    New bps registered to app

---
+++ alerts/alerts_sched.py
@@ -0,0 +1,26 @@
+import flask
+from flask_debug import Debug
+import math
+import time
+import json
+import random
+import string
+import datetime
+import threading
+from jinja2 import StrictUndefined
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, jsonify, Blueprint)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import (update, asc, desc)
+from model import User, Contact, AlertSet, Alert, CheckIn, ReqCheck, connect_to_db, db
+import requests
+import logging
+
+from functools import wraps
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+from dotenv import load_dotenv, find_dotenv
+
+from auth import requires_auth
+
+alerts_sched_bp = Blueprint('alerts_sched_bp', __name__)
