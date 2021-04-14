a04d8c808c26d4f19242af9decb73bc48ea5e6ae
beegeiger <beegeiger@gmail.com>
2021-04-14 15:57:50 -0700

    Still Reworking old Alertset paths

--- Components/alerts.py
+++ Components/alerts.py
@@ -88,3 +88,78 @@ def deactivate_alertset(alert_id):
         {'active': False, 'checked_in': False,'datetime': None})
     db.session.commit()
     return "Alert Set Deactivated"
+
+@alerts_bp.route("/add_alert", methods=["POST"])
+def add_alert():
+    """Adds a recurring Alert-Set to the dBase"""
+    user = User.query.filter_by(email=session['current_user']).one()
+    alerts_all = Alert.query.filter_by(user_id=user.user_id).all()
+
+    #Gets the alert and alert set info from the form on the add a new rec set page
+    name = request.form['set_nam']
+    desc = request.form['descri']
+    interval = request.form['interval']
+    contacts = request.form.getlist('contact')
+    time = request.form['time']
+    print("name1: ", name, type(name), len(name))
+
+    if len(name)== 0:
+        name = "Alert " + str(len(alert_sets_all))
+    print("name2: ", name, type(name), len(name))
+    #Queries the current user
+
+    dt = datetime.datetime.now()
+
+    #Initiates 3 contact variables, sets the first to the first contact and the next two to None
+    contact1 = int(contacts[0])
+    contact2 = None
+    contact3 = None
+
+    #If more than one contact is associated with the alert set, the following variables are set to them
+    if len(contacts) > 1:
+        contact2 = int(contacts[1])
+    if len(contacts) > 2:
+        contact3 = int(contacts[2])
+
+    #A new alert (associated with the alert set) is created, added, and commited to the dBase
+    new_alert = Alert(user_id=user.user_id, contact_id1=contact1,
+                      contact_id2=contact2, contact_id3=contact3, interval=interval, message=desc, time=time)
+
+    db.session.add(new_alert)
+    db.session.commit()
+
+    return redirect("/bs_alerts")
+
+
+@alerts_bp.route("/save_alert/<alert_id>", methods=["POST"])
+def save_recset(alert_id):
+    """Saves the edits to a recurring alert set"""
+
+    #Gets the alert and alert set info from the form
+    name = request.form['set_name']
+    desc = request.form['descri']
+    interval = request.form['interval']
+    contacts = request.form.getlist('contact')
+
+    #The Alert-Set is updated in the dBase with the new data
+    (db.session.query(AlertSet).filter_by(alert_set_id=alert_set_id)).update(
+    {'a_name': name, 'a_desc': desc, 'interval': interval})
+
+    #Initiates 3 contact variables, sets the first to the first contact and the next two to None
+    contact1 = int(contacts[0])
+    contact2 = None
+    contact3 = None
+
+    #If more than one contact is associated with the alert set, the following variables are set to them
+    if len(contacts) > 1:
+        contact2 = int(contacts[1])
+    if len(contacts) > 2:
+        contact3 = int(contacts[2])
+
+    #The alert associated with the alert set is then updated and all of the changes are committed
+    (db.session.query(Alert).filter_by(alert_set_id=alert_set_id)).update(
+    {'message': desc, 'interval': interval, 'contact_id1': contact1, 'contact_id2': contact2, 'contact_id3': contact3})
+    db.session.commit()
+
+    #The user is then re-routed to the main besafe page
+    return redirect("/bs_alerts")
