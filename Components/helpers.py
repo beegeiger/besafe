da104b1c2910207614f1e5cf3d3e4b0a76dc1ed3
beegeiger <36778471+beegeiger@users.noreply.github.com>
2021-10-04 15:20:35 -0700

    Update helpers.py

--- Components/helpers.py
+++ Components/helpers.py
@@ -54,7 +54,7 @@ def check_in(user_id, notes):
     db.session.commit()
     return "Check In has been Logged!"
 
-def create_message(alert_id):
+def create_alert(alert_id):
     """Helper Function for creating an alert's actual message body"""
 
     #Datetime object for now created for convenience
@@ -193,7 +193,7 @@ def check_alerts():
                 #If there is no check-in and the alert is within a minute, an alert is sent
                 if abs(difference) <= datetime.timedelta(minutes=1) and abs(difference) > datetime.timedelta(seconds=0) and checks == 0 and alert.sent == False:
                     print('A CHECK-IN WAS MISSED AND AN ALERT IS BEING SENT NOW!')
-                    message_body = create_message(alert.alert_id)
+                    message_body = create_alert(alert.alert_id)
                     send_alert_contacts(alert.alert_id, message_body)
                     #The alert object is updates to be marked sent and inactive and its commited
                     (db.session.query(Alert).filter_by(alert_id=alert.alert_id)).update({'sent': True, 'active': False})
